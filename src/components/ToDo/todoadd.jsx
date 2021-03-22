7e0348c37ce547f0d6128451b19a7084c0ad7a45
Sarah <sarah.ali2810@gmail.com>
2021-03-22 17:44:43 +0200

    updating todo app

--- src/components/ToDo/todoadd.jsx
+++ src/components/ToDo/todoadd.jsx
@@ -5,16 +5,17 @@ class ToDoAdd extends Component {
     state = {  
         Task:'', 
     }
-    addTask=(e)=>{
-        this.setState({Task:e.target.value});
+    addTask=()=>{
+        this.props.onAdd(this.state.Task);
+        this.setState({Task:''});
     }
     render() { 
 
         return ( 
             <React.Fragment>
                <h5>Add New Task</h5> 
-               <input className="m-2" type="text" placeholder="Task name" onChange={this.addTask} />
-               <button className="btn btn-success m-2" onClick={()=>this.props.onAdd(this.state.Task)}>Add</button>
+               <input className="m-2" type="text" placeholder="Task name" value={this.state.Task} onChange={(e)=>this.setState({Task:e.target.value})} />
+               <button className="btn btn-success m-2" onClick={this.addTask}>Add</button>
             </React.Fragment>
         );
     }
